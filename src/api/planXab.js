559dd7fb77298b4b170d4c2846b65ad7e24ae9a3
byambasuren.e <byambasuren.e@erdenetmc.mn>
2023-04-20 09:09:22 +0800

    tt

--- src/api/planXab.js
+++
@@ -1,6 +0,0 @@
-import { API } from "src/api/request";
-
-// export async function getLessonType() {
-//     const response = await API().get("ref/lesson/type");
-//     return response.data;
-//   }
