7da653327353fcfbe3f2987c74a73d6a54c95a0b
dmunkh <dmunkh84@gmail.com>
2024-06-16 21:20:51 +0800

    dmunkh

--- src/pages/order/modal_add_baraa.js
+++ src/pages/order/modal_add_baraa.js
@@ -146,8 +146,8 @@ const ModalNormDetail = () => {
         setLoading(true);
         axios
 
-          .post("https://dmunkh.store/api/backend/balance", {
-            // .post("http://localhost:5000/api/backend/balance", {
+          // .post("https://dmunkh.store/api/backend/balance", {
+          .post("http://localhost:5000/api/backend/balance", {
             type_id: 3,
             baraa_id: baraa[0].id,
             baraa_ner: baraa[0].baraa_ner,
